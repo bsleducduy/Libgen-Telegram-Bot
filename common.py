b2f462b1f1130c3902e9e916ee4fefe9def13c22
bsleducduy <85248617+bsleducduy@users.noreply.github.com>
2021-09-13 09:12:08 +0700

    Update common.py

--- common.py
+++ common.py
@@ -5,7 +5,7 @@ import logging
 
 mode = "prod" 
 TELEGRAM_ACCESS_TOKEN = "1828250945:AAG8mxez-5ufyPwlikLAzl1A8wyoUQT409I"
-HEROKU_APP_NAME = ""
+HEROKU_APP_NAME = "libgentelegrambot123"
 LIBGEN_DOMAIN = "https://libgen.is/"
 
 # Enable logging
